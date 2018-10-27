f9684f5d9476225e3597c123dfeff2609aac2a39
Linuxx nexus <laredkevoh@gmail.com>
2018-10-27 13:07:01 +0300

    Add files via upload

---
+++ git-challenge/calculate_pay.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def get_perf_pay(readers):
+    if readers < 200:
+        ret
+    if readers <= 400:
+        return read
+    return 100 + (readers - 400) * 0.03
+
+
+if __name__ == "__main__":
+    if (len(sys.argv)) < 2:
+        print('Plea
+    else:
+        print('Total pay:')
+        print(get_perf_pay(int(sys.argv[1])))
